c78666254ea093e402d048d463391fac400c135f
Mediashare <mediashare.supp@gmail.com>
2020-12-13 21:27:25 +0100

    time-tracking.php

--- bin/time-tracking.php
+++ bin/time-tracking.php
@@ -1,15 +1,19 @@
 #!/usr/bin/env php
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+
 $app = new Symfony\Component\Console\Application('Time-Tracking', '0.2.0');
-$app->add((new Mediashare\Command\TimerListCommand('timer:list')));
-$app->add((new Mediashare\Command\TimerStartCommand('timer:start')));
-$app->add((new Mediashare\Command\TimerStopCommand('timer:stop')));
-$app->add((new Mediashare\Command\TimerStatusCommand('timer:status')));
-$app->add((new Mediashare\Command\TimerEndCommand('timer:end')));
-$app->add((new Mediashare\Command\TimerRemoveCommand('timer:remove')));
-$app->add((new Mediashare\Command\CommitCommand('timer:commit')));
-$app->add((new Mediashare\Command\CommitEditCommand('timer:commit:edit')));
-$app->add((new Mediashare\Command\CommitRemoveCommand('timer:commit:remove')));
-$app->add((new Mediashare\Command\UpgradeCommand('upgrade')));
+$commandsLoader = new \Symfony\Component\Console\CommandLoader\FactoryCommandLoader([
+    'timer:list' => function () { return new Mediashare\Command\TimerListCommand(); },
+    'timer:start' => function () { return new Mediashare\Command\TimerStartCommand(); },
+    'timer:stop' => function () { return new Mediashare\Command\TimerStopCommand(); },
+    'timer:status' => function () { return new Mediashare\Command\TimerStatusCommand(); },
+    'timer:end' => function () { return new Mediashare\Command\TimerEndCommand(); },
+    'timer:remove' => function () { return new Mediashare\Command\TimerRemoveCommand(); },
+    'timer:commit' => function () { return new Mediashare\Command\CommitCommand(); },
+    'timer:commit:edit' => function () { return new Mediashare\Command\CommitEditCommand(); },
+    'timer:commit:remove' => function () { return new Mediashare\Command\CommitRemoveCommand(); },
+    'upgrade' => function () { return new Mediashare\Command\UpgradeCommand(); },
+]);
+$app->setCommandLoader($commandsLoader);
 $app->run();
